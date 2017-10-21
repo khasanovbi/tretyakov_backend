31ed9a4c30e13cac6831cb166f7b018b7922cb59
Zdanchuk Andrey <a.zdanchuk@rambler-co.ru>
2017-10-21 22:46:03 +0300

    tf

--- scripts/label_image.py
+++ scripts/label_image.py
@@ -70,47 +70,15 @@ def load_labels(label_file):
     return label
 
 
-if __name__ == "__main__":
-    file_name = "tf_files/flower_photos/daisy/3475870145_685a19116d.jpg"
+def main(file_name):
     model_file = "tf_files/retrained_graph.pb"
-    label_file = "tf_files/retrained_labels.txt"
     input_height = 224
     input_width = 224
     input_mean = 128
     input_std = 128
     input_layer = "input"
     output_layer = "final_result"
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--image", help="image to be processed")
-    parser.add_argument("--graph", help="graph/model to be executed")
-    parser.add_argument("--labels", help="name of file containing labels")
-    parser.add_argument("--input_height", type=int, help="input height")
-    parser.add_argument("--input_width", type=int, help="input width")
-    parser.add_argument("--input_mean", type=int, help="input mean")
-    parser.add_argument("--input_std", type=int, help="input std")
-    parser.add_argument("--input_layer", help="name of input layer")
-    parser.add_argument("--output_layer", help="name of output layer")
-    args = parser.parse_args()
-
-    if args.graph:
-        model_file = args.graph
-    if args.image:
-        file_name = args.image
-    if args.labels:
-        label_file = args.labels
-    if args.input_height:
-        input_height = args.input_height
-    if args.input_width:
-        input_width = args.input_width
-    if args.input_mean:
-        input_mean = args.input_mean
-    if args.input_std:
-        input_std = args.input_std
-    if args.input_layer:
-        input_layer = args.input_layer
-    if args.output_layer:
-        output_layer = args.output_layer
+    label_file = "tf_files/retrained_labels.txt"
 
     graph = load_graph(model_file)
     t = read_tensor_from_image_file(file_name,
@@ -121,8 +89,8 @@ if __name__ == "__main__":
 
     input_name = "import/" + input_layer
     output_name = "import/" + output_layer
-    input_operation = graph.get_operation_by_name(input_name);
-    output_operation = graph.get_operation_by_name(output_name);
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
 
     with tf.Session(graph=graph) as sess:
         results = sess.run(output_operation.outputs[0],
@@ -131,4 +99,4 @@ if __name__ == "__main__":
 
     top_k = results.argsort()[-5:][::-1]
     labels = load_labels(label_file)
-    print(labels[top_k[0]])
+    return labels[top_k[0]]
